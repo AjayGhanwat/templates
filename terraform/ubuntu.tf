600e2e805b416cd70c5c1e4d8023d0a2f1182adf
Ajay Ghanwat <ghanwat.ajay@workroot.in>
2024-10-06 20:00:24 +0530

    Updated Terraform VM Createtion File

--- terraform/ubuntu.tf
+++ terraform/ubuntu.tf
@@ -1,8 +1,8 @@
-resource "proxmox_vm_qemu" "coolify_resource" {
+resource "proxmox_vm_qemu" "dns" {
     target_node = "pve"
     vmid = 102
-    name = "CoolifyResource"
-    desc = "Coolify Resource Server"
+    name = "DNS"
+    desc = "DNS Server"
 
     onboot = true 
     boot = "order=scsi0"
@@ -56,11 +56,11 @@ resource "proxmox_vm_qemu" "coolify_resource" {
 
     os_type = "cloud-init"
 
-    ipconfig0 = "ip=192.168.0.107/24,gw=192.168.0.1"
+    ipconfig0 = "ip=192.168.0.104/24,gw=192.168.0.1"
     nameserver = "192.168.0.1"
 
-    ciuser = "coolify"
-    cipassword = "coolify"
+    ciuser = "dns"
+    cipassword = "dns"
 
     sshkeys = <<EOF
     ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABgQDIcwVHhxKi48DxzVQQQcTGTsR3qzCKIButh4+qfLCjr76HvTgmHMhR2BOM4SJhxeroloeHPPcqk7L4sxkh+k+WKZ4vj2FJ/Sd3kjzZvJa6VWB5/g2+kmlkQAjmq18HK9n7QwQo229C0QIp29F/Sk2i1d0bodXkEWYHfiJxSUxA/KNL/d8++z9akbxZv1x7Y8D56cOz+FIeMPtO3QscOLCUKgMKCsRY8F2KgyfO+3slB6B5Rrxlw53dQBzW1RPKacN3T6t35JdsisaD3bh7+FIAcNHryokkzjBS4iw/TSmAMxrUnynBxSQFTNiDBvFn0MZQix+ZSLhQqEqGuKPpyXN9MEJOYMaurZH63d7aXnlK5Ul/If6e3wactkgV0/EmSWRyd4/1bNWsFH7tBYnKL8B35uEmNGP6fLYEd18+MFflchtNTq1KouU/FxBqlitJ3L4nPmGkey0ssN1Nzg135mFenWSTXCq7An6eAZqSvUPPJMgswxw0YSi0V9TeIny3ZFU= admin@DESKTOP-OHGNB46
